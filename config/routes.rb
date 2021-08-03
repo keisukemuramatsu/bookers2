50f5014cd11c2cfaf23b794d7ed12a15c1ebeb04
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-08-03 14:35:01 +0000

    [Add]BooksEdit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :books
   get 'users/show'
   get 'homes/about'
   root to: "homes#top"
   devise_for :users
-  resources :books, only: [:create, :index, :show, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
