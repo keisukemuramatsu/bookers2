900c8b42c13805c067d8794bdd096e77748a0226
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-08-05 13:11:07 +0000

    [Add]booksレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+  end
+  
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
   
   def show
@@ -25,4 +34,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy  
   attachment :profile_image
   validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
-  validates :name, presence: true#追記
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: { maximum: 50 } #追記
 end
